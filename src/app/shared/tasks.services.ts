0e10907a5d50a6cb728e074c11b9ac6dbd14aa03
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-28 21:05:03 +0545

    implement func to get all tasks and get task by id

--- src/app/shared/tasks.services.ts
+++ src/app/shared/tasks.services.ts
@@ -8,8 +8,16 @@ export class TasksServices{
   private tasks = signal<Task[]>([]);
   private httpClient = inject(HttpClient);
 
-  fetchData(url: string, fetchError: string){
-    this.httpClient.get<Task[]>(url).pipe(
+  getAllTasks(){
+    return this.fetchData('http://localhost:8080/apis/tasks', 'sorry! could not load tasks!');
+  }
+  getTaskById(id: number){
+    return this.fetchData(`http://localhost:8080/apis/tasks/${id}`, 'sorry! could not load selected task!');
+  }
+
+
+  private fetchData(url: string, fetchError: string){
+    return this.httpClient.get<Task[]>(url).pipe(
       catchError(error=> throwError(()=> new Error(fetchError)))
     );
   }
