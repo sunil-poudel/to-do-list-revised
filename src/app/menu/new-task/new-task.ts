d83e79bad292230e5538a953b9233510f714ba43
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-28 23:20:51 +0545

    successfully edit task, but ui wont update right away

--- src/app/menu/new-task/new-task.ts
+++ src/app/menu/new-task/new-task.ts
@@ -1,6 +1,6 @@
 import {Component, effect, EventEmitter, inject, Input, Output, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {TaskData} from '../../shared/shared';
+import {TaskData, TaskValue} from '../../shared/shared';
 import {TasksServices} from '../../shared/tasks.services';
 
 @Component({
@@ -25,12 +25,30 @@ export class NewTask {
   }
 
   onSubmit() {
-    if(this.enteredTitle!=''){
-      const taskData: TaskData = {title: this.enteredTitle, date: this.enteredDate, description: this.enteredDescription};
-      this.tasksService.addNewTask(taskData).subscribe({
-        next: task => console.log('added task: ', task)
-      });
+    if(this.enteredTitle!='') {
+      if (this.tasksService.displayAddMenu()) {
+        const taskData: TaskData = {
+          title: this.enteredTitle,
+          date: this.enteredDate,
+          description: this.enteredDescription
+        };
+        this.tasksService.addNewTask(taskData).subscribe({
+          next: task => console.log('added task: ', task)
+        });
+      } else if (this.tasksService.displayEditMenu()) {
+        const taskValue: TaskValue = {
+          id: this.tasksService.currentTaskId(),
+          title: this.enteredTitle,
+          date: this.enteredDate,
+          description: this.enteredDescription
+        }
+        this.tasksService.editTheTask(taskValue).subscribe({
+          next: task=> console.log('edited task: '+task)
+        });
+      }
+      this.tasksService.displayAddMenu.set(false);
+      this.tasksService.displayEditMenu.set(false);
     }
-    this.tasksService.displayAddMenu.set(false);
+
   }
 }
