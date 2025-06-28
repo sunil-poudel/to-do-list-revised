9fe33fadb27b372a64f76e1f416f1094caf2ad2e
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-28 21:58:57 +0545

    display add menu

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,16 +1,14 @@
 import {Component, EventEmitter, Output, inject} from '@angular/core';
-import {NewTask} from './new-task/new-task';
-import {TaskData} from '../shared/shared'
-import {TasksServices} from '../shared/tasks.services';
 
 @Component({
   selector: 'app-menu',
   imports: [
-    NewTask
   ],
   templateUrl: './menu.html',
   styleUrl: './menu.css'
 })
 export class Menu {
+  onClickAdd(){
 
+  }
 }
