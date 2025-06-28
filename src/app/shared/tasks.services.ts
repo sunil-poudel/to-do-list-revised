4c18822efded2469e498718561bab71d9b988efc
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-28 21:01:16 +0545

    func to fetch data from url

--- src/app/shared/tasks.services.ts
+++ src/app/shared/tasks.services.ts
@@ -1,11 +1,18 @@
 import {TaskData, Task} from './shared';
 import {inject, Injectable, signal} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {catchError, throwError} from 'rxjs';
 
 @Injectable({providedIn: "root"})
 export class TasksServices{
   private tasks = signal<Task[]>([]);
   private httpClient = inject(HttpClient);
+
+  fetchData(url: string, fetchError: string){
+    this.httpClient.get<Task[]>(url).pipe(
+      catchError(error=> throwError(()=> new Error(fetchError)))
+    );
+  }
 
 
 }
